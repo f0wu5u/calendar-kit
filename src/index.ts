b617381780fa4b9cef2d2a293aee4bac179e949c
Frank Owusu-Agyemang <codewithfrank@gmail.com>
2024-06-02 21:56:23 +0200

    chore: added useRender count hook and library entry file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./components";
+export { useRenderCount } from "./hooks/useRenderCount";
+export * from "./types";
+export { dateStringToUTCDate, toUTCDate, toUTCDateString } from "./utils/date";
+export { getDayState } from "./utils/getDayState";
