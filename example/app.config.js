75f334c501285c889d8b7d6166b2779f75884e3d
Frank Owusu-Agyemang <codewithfrank@gmail.com>
2024-12-15 17:19:25 +0100

    chore(readme): updated readme

--- example/app.config.js
+++ example/app.config.js
@@ -2,7 +2,7 @@ export default ({ config }) => ({
   ...config,
   name: "React Native Calendar Kit",
   slug: "@fowusu/calendar-kit",
-  "newArchEnabled": true,
+  newArchEnabled: true,
   extra: {
     storybookEnabled: process.env.STORYBOOK_ENABLED,
     supportsRTL: true,
