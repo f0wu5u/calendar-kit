0cc55ee2d0edce57372f6fa3546a3cd5c131786d
Frank Owusu-Agyemang <codewithfrank@gmail.com>
2024-12-15 16:46:27 +0100

    chore(example): updated expo

--- example/app.config.js
+++ example/app.config.js
@@ -2,6 +2,7 @@ export default ({ config }) => ({
   ...config,
   name: "React Native Calendar Kit",
   slug: "@fowusu/calendar-kit",
+  "newArchEnabled": true,
   extra: {
     storybookEnabled: process.env.STORYBOOK_ENABLED,
     supportsRTL: true,
